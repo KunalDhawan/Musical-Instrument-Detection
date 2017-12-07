91d04f29a4a2ae2c91f2d818c8efbd7356b09af0
KunalDhawan <kunaldhawan97@gmail.com>
2017-12-07 11:46:09 +0530

    Add files via upload

---
+++ Classifier/family_classifier/main.py
@@ -0,0 +1,62 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, BatchNormalization
+import numpy as np
+import cPickle
+
+f = open('../../data/familywise/data.pkl','rb')
+[x_train,y_train,x_test,y_test] = cPickle.load(f)
+f.close()
+print(x_train.shape,  y_train.shape, x_test.shape, y_test.shape)
+batch_size = y_train.shape[0]
+num_classes = 3
+epochs = 250
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+mean = np.mean(x_train, 0)
+sd = np.std(x_train,0)
+
+x_train = (x_train - mean)/sd
+x_test = (x_test - mean)/sd
+
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(1024, input_shape=(8,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.8))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_classes, activation='softmax'))
+
+opt = keras.optimizers.adam(lr=0.001, beta_1=0.9, beta_2=0.999,epsilon=1e-8)
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1)#,
+                    #validation_data=(x_test, y_test))
+
+model.save_weights("temp.h5")
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:'+ str(score[0]))
+print('Test accuracy:'+ str(score[1]))
